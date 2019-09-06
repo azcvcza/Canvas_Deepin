63cd5fc43c78a4ef0fe70123e887e66960a7b331
azcvcza <283940802@qq.com>
2019-09-06 10:06:23 +0800

    没找到iconv-lite，只是打印了拼接的buffer

--- js/buffer-concat.js
+++ js/buffer-concat.js
@@ -1,10 +1,17 @@
 var fs = require('fs');
+//var iconv = require('iconv');
 var rs = fs.createReadStream('../README.md',{highWaterMark:11});
-rs.setEncoding('utf8');
+//rs.setEncoding('utf8');
+var chunks = [];
+var size = 0;
 var data = '';
 rs.on('data', function(chunk){
-    data += chunk;// data = data.toString() + chunk.toString();
+    //data += chunk;// data = data.toString() + chunk.toString();
+    chunks.push(chunk);
+    size += chunk.length;
 })
 rs.on('end', function(){
-    console.log('end,data:\n',data);
+    var buf = Buffer.concat(chunks,size);
+    //var str = iconv.decode(buf,'utf8') can't find module
+    console.log('end,str:\n',buf);
 })
